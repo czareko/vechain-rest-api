2c1afc538897f2cd4cc9108b5e8616dec96c87b2
cezary.olborski <cezary.olborski@trinite.io>
2021-04-04 14:53:06 +0800

    Basic route configuration

--- app.ts
+++ app.ts
@@ -4,11 +4,13 @@ import * as bodyparser from 'body-parser';
 import * as winston from 'winston';
 import * as expressWinston from 'express-winston';
 import cors from 'cors'
+import {CommonRoutesConfig} from './common/common.routes.config';
 import debug from 'debug';
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
-const port = 3000;
+const port = 8080;
+const routes: Array<CommonRoutesConfig> = [];
 const debugLog: debug.IDebugger = debug('app');
 
 app.use(bodyparser.json());
@@ -40,4 +42,7 @@ app.get('/', (req: express.Request, res: express.Response) => {
 });
 server.listen(port, () => {
     debugLog(`Server running at http://localhost:${port}`);
+    routes.forEach((route: CommonRoutesConfig) => {
+        debugLog(`Routes configured for ${route.getName()}`);
+    });
 });
